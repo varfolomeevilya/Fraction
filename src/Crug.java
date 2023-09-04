0f9a0396579a677efed65c56620bcade4e66c606
ilya <varfolomeevi83@gmail.com>
2023-09-04 11:32:50 +0300

    задача про круг

---
+++ src/Crug.java
@@ -0,0 +1,19 @@
+public class Crug {
+    private int radius=4;
+    public int area;
+    private int perimeter=314;
+
+    public int getPerimeter(){
+         return perimeter=2*perimeter*radius;
+    }
+    public int getArea(){
+         return area=perimeter*radius*2;
+    }
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+    public static void main(String[] args) {
+     Crug crug= new Crug();
+     crug.getRadius();
+     System.out.println(crug.getRadius());
+     crug.setRadius(4);
+     System.out.println("Подсчёт периметра " + crug.getPerimeter());
+     System.out.println("Подсчёт площади "   +  crug.getArea());
+     crug.getPerimeter();
+     System.out.println(crug.getPerimeter());
+
+
+    }
+}
