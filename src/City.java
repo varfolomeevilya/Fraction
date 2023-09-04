28e7e37cbafd93fe5eb9a016db9a533ecb666f57
ilya <varfolomeevi83@gmail.com>
2023-09-04 11:33:48 +0300

    задача про круг

---
+++ src/City.java
@@ -0,0 +1,64 @@
+public class City {
+    private String Phone_code_of_the_city;
+    private String Name_of_the_city;
+    private  String Name_of_the_region;
+    private String Number_of_residents_in_the_city;
+    private String Country_name;
+    private String Postal_code_of_the_city;
+
+    public String getPhone_code_of_the_city() {
+        return Phone_code_of_the_city;
+    }
+
+    public void setPhone_code_of_the_city(String phone_code_of_the_city) {
+        this.Phone_code_of_the_city = phone_code_of_the_city;
+    }
+
+    public String getPostal_code_of_the_city() {
+        return Postal_code_of_the_city;
+    }
+
+    public void setPostal_code_of_the_city(String postal_code_of_the_city) {
+       this. Postal_code_of_the_city = postal_code_of_the_city;
+    }
+
+
+
+    public String getNumber_of_residents_in_the_city() {
+        return Number_of_residents_in_the_city;
+    }
+
+    public void setNumber_of_residents_in_the_city(String number_of_residents_in_the_city) {
+        this.Number_of_residents_in_the_city = number_of_residents_in_the_city;
+    }
+
+
+    public String getCountry_name() {
+        return Country_name;
+    }
+
+    public void setCountry_name(String country_name) {
+       this. Country_name = country_name;
+    }
+
+
+
+    public String getName_of_the_region() {
+        return Name_of_the_region;
+    }
+
+    public void setName_of_the_region(String name_of_the_region) {
+       this.Name_of_the_region = name_of_the_region;
+    }
+
+
+    public String getName_of_the_city() {
+        return Name_of_the_city;
+    }
+
+    public void setName_of_the_city(String name_of_the_city) {
+        this.Name_of_the_city = name_of_the_city;
+    }
+
+
+}
---
+++ src/Task02.java
@@ -0,0 +1,29 @@
+public class Task02 {
+    public static void main(String[] args){
+        City city =new City();
+        city.setCountry_name("Россия");
+
+        city.setName_of_the_city("Рязань");
+
+        city.setNumber_of_residents_in_the_city("120 человек");
+
+        city.setName_of_the_region("777- москва, 161 - Ростов на Дону, 62 - Рязань");
+
+        city.setPhone_code_of_the_city("4912-Разань");
+
+        city.setPostal_code_of_the_city("390007 - улица октябрьского городка");
+
+        System.out.println( city.getCountry_name());
+
+        System.out.println(city.getName_of_the_city());
+
+        System.out.println(city.getNumber_of_residents_in_the_city());
+
+        System.out.println(city.getName_of_the_region());
+
+        System.out.println(city.getPhone_code_of_the_city());
+
+        System.out.println(city.getPostal_code_of_the_city());
+
+    }
+}
